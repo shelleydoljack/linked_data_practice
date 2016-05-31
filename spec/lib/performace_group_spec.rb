f632f44f2b6544a4a6a01bfe6ea46ecfdeea2ecb
Shelley Doljack <sdoljack@stanford.edu>
2016-05-31 15:37:21 -0700

    Remove test file.

--- spec/lib/performace_group_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'PerformanceGroup'
-
-describe 'PerformanceGroup' do 
-	it 'can instantiate' do
-		expect(PerformanceGroup.new).to be_kind_of(PerformanceGroup)
-	end
-end
