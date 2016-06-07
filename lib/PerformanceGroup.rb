8a4f0998537491fd7293850c0c9a2c933348b73e
Shelley Doljack <sdoljack@stanford.edu>
2016-06-07 14:29:07 -0700

    Add test for name attribute.

--- lib/PerformanceGroup.rb
+++ lib/PerformanceGroup.rb
@@ -1,3 +1,3 @@
 class PerformanceGroup
-
+	attr_accessor :name
 end
--- spec/lib/performance_group_spec.rb
+++ spec/lib/performance_group_spec.rb
@@ -5,4 +5,9 @@ describe 'PerformanceGroup' do
 	it 'can instantiate' do
 		expect(PerformanceGroup.new).to be_kind_of(PerformanceGroup)
 	end
+	it 'has a name' do
+		foo = PerformanceGroup.new
+		foo.name = 'The Beatles'
+		expect(foo.name).to eq 'The Beatles'
+	end
 end
